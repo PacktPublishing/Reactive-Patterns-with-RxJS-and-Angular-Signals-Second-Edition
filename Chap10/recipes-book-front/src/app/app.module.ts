a763f9165b9c3e8419291fe6fcf6c9bcbb7bb466
lamisChebbi <chebbi.lamis@gmail.com>
2024-07-14 19:02:15 +0100

    feat(): Update Angular 18

--- Chap10/recipes-book-front/src/app/app.module.ts
+++ Chap10/recipes-book-front/src/app/app.module.ts
@@ -2,23 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { RecipesService } from './core/services/recipes.service';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './core/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HeaderComponent
-  ],
-  providers: [RecipesService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        HeaderComponent], providers: [RecipesService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
